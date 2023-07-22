ed8a5857d5ae744a605a255e3c50111113a8c82b
Filipe Morgado <filipe.morgado@wit-software.com>
2023-07-22 20:21:36 +0100

    - Settings up app splash screen

---
+++ app/src/main/java/com/filipemorgado/myexpenses/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.filipemorgado.myexpenses
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.filipemorgado.myexpenses.databinding.ActivitySplashMainBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Add a delay of 2000 milliseconds (2 seconds) before starting MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Close the splash screen activity so it's not accessible via the back button
+        }, 2000)
+    }
+}
