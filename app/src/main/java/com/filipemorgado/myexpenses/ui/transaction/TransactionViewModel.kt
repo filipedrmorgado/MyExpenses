8b277fc8b6817f258cb639e24f9fe89b046eb0ee
Filipe Morgado <filipe.morgado@wit-software.com>
2023-07-24 01:11:52 +0100

    - Added bottomViewIcons - Reordered bottom_nav_menu.xml

--- app/src/main/java/com/filipemorgado/myexpenses/ui/transaction/TransactionViewModel.kt
+++ app/src/main/java/com/filipemorgado/myexpenses/ui/transaction/TransactionViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class TransactionViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is transaction Fragment"
     }
     val text: LiveData<String> = _text
 }
