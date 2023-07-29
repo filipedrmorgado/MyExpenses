09d989a2883cf21b034dcc766a625392a90d69b1
Filipe Morgado <filipe.morgado@wit-software.com>
2023-07-29 23:57:06 +0100

    - Updating days selector and tryout selectors

--- app/src/main/java/com/filipemorgado/myexpenses/ui/home/HomeFragment.kt
+++ app/src/main/java/com/filipemorgado/myexpenses/ui/home/HomeFragment.kt
@@ -7,10 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.filipemorgado.myexpenses.databinding.FragmentHomeBinding
+import com.filipemorgado.myexpenses.databinding.TransactionsTimeSelectorBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var daySelectorBinding: TransactionsTimeSelectorBinding
     private val homeViewModel: HomeViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -19,8 +21,19 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        daySelectorBinding = TransactionsTimeSelectorBinding.bind(binding.daySelector.root)
+
+
+        setRadioButtonSelectors()
+
         return binding.root
     }
+
+    private fun setRadioButtonSelectors() {
+
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
