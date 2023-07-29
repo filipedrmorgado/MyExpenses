e96b4ae34d51334bb291bbfe2184cc1f26597e59
Filipe Morgado <filipe.morgado@wit-software.com>
2023-07-30 00:25:50 +0100

    - Days selector aspect - Remove unused files

--- app/src/main/java/com/filipemorgado/myexpenses/ui/home/HomeFragment.kt
+++ app/src/main/java/com/filipemorgado/myexpenses/ui/home/HomeFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.filipemorgado.myexpenses.R
 import com.filipemorgado.myexpenses.databinding.FragmentHomeBinding
 import com.filipemorgado.myexpenses.databinding.TransactionsTimeSelectorBinding
 
@@ -30,10 +32,27 @@ class HomeFragment : Fragment() {
     }
 
     private fun setRadioButtonSelectors() {
+        // Set a listener to the RadioGroup to detect selection changes
+        daySelectorBinding.radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            // Set the font family for all RadioButtons
+            setFontFamilyToRadioButtons()
+        }
 
+        // Set the initial font family based on the default selected RadioButton (if any)
+        setFontFamilyToRadioButtons()
     }
 
-
+    private fun setFontFamilyToRadioButtons() {
+        for (i in 0 until daySelectorBinding.radioGroup.childCount) {
+            val radioButton = daySelectorBinding.radioGroup.getChildAt(i) as? RadioButton
+            val font = if (radioButton?.isChecked == true) {
+                resources.getFont(R.font.inter_bold) // Replace with the font resource for the selected state
+            } else {
+                resources.getFont(R.font.inter_medium) // Replace with the font resource for the selected state
+            }
+            radioButton?.typeface = font
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
